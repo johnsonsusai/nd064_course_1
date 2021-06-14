a53bc4301a8044e9acf4f8068597588f101d633f
Johnson Susai <jsusai@outlook.com>
2021-06-14 15:58:31 -0400

    update

--- exercises/python-helloworld/app.py
+++ exercises/python-helloworld/app.py
@@ -3,14 +3,14 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello Worldd!"
+    return "Hello Worlddd!"
 
 @app.route("/status")
 #def status():
-    value = {
+    valu = {
         "user": "admin", "statuscode": "200"
     }
-    return json.dumps(value)
+    return json.dumps(valu)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
